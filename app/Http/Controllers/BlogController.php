752e5889d4aa9a483d232df1dfae6e7ab95c6845
Giorgi Tsukhishvili <gcuxishvili@gmail.com>
2023-06-23 22:03:11 +0400

    feat: add blog routes and functions

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,11 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Models\Blog;
+use Illuminate\Http\JsonResponse;
 
 class BlogController extends Controller
 {
-	public function index()
+	public function index(): JsonResponse
 	{
-		return response()->json(['blogs' => Blog::all()]);
+		$blogs = Blog::with('user')->get();
+
+		return response()->json(['blogs' => $blogs]);
+	}
+
+	public function store()
+	{
+	}
+
+	public function put(Blog $blog)
+	{
+	}
+
+	public function destroy(Blog $blog): JsonResponse
+	{
+		$blog->delete();
+
+		return response()->json(['message' => 'Blog deleted successfully']);
 	}
 }
--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(BlogController::class)->group(function () {
 	Route::get('/', 'index')->name('blog.index');
+	Route::post('/', 'store')->name('blog.store');
+	Route::put('/{blog}', 'put')->name('blog.put');
+	Route::delete('/{blog}', 'destroy')->name('blog.destroy');
 });
