f1588560b32224678f00b8705308ee873af2d315
Giorgi Tsukhishvili <gcuxishvili@gmail.com>
2023-06-24 12:57:06 +0400

    feat: add ability to fetch all tags

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -2,6 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tag;
+use Illuminate\Http\JsonResponse;
+
 class TagController extends Controller
 {
+	public function index(): JsonResponse
+	{
+		$tags = Tag::all('id', 'name');
+
+		return response()->json(['tags' => $tags]);
+	}
 }
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,19 @@
 <?php
 
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(BlogController::class)->group(function () {
-	Route::get('/', 'index')->name('blog.index');
-	Route::post('/', 'store')->name('blog.store');
-	Route::put('/{blog}', 'put')->name('blog.put');
-	Route::delete('/{blog}', 'destroy')->name('blog.destroy');
+	Route::prefix('blogs')->group(function () {
+		Route::get('/', 'index')->name('blog.index');
+		Route::get('/{blog}', 'show')->name('blog.show');
+		Route::post('/', 'store')->name('blog.store');
+		Route::put('/{blog}', 'put')->name('blog.put');
+		Route::delete('/{blog}', 'destroy')->name('blog.destroy');
+	});
+});
+
+Route::controller(TagController::class)->group(function () {
+	Route::get('/tags', 'index')->name('tag.index');
 });
