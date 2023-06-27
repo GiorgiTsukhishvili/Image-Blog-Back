a370c7e2e49cc1edce46553f2c7a193930f8d382
Giorgi Tsukhishvili <gcuxishvili@gmail.com>
2023-06-27 21:16:10 +0400

    fix: add extra columns to users table: google id and image

--- app/Http/Controllers/SubscribeController.php
+++ app/Http/Controllers/SubscribeController.php
@@ -4,16 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Models\Subscribe;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\Auth;
 
 class SubscribeController extends Controller
 {
 	public function index(): JsonResponse
 	{
-		Auth::loginUsingId(1);
-
 		$subscribers = Subscribe::where('user_id', auth()->user()->id)
-		->with('subscribes:id,name')
+		->with('subscribes:id,name,image')
 		->get(['id', 'subscribed_id']);
 
 		return response()->json(['subscribers' => $subscribers], 200);
