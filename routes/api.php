d9637190ab97903d2e1d6b6c0d08c28636eb87c8
Giorgi Tsukhishvili <gcuxishvili@gmail.com>
2023-07-23 16:29:31 +0400

    feat: group routes by sanctum middlware

--- routes/api.php
+++ routes/api.php
@@ -19,9 +19,11 @@ Route::controller(BlogController::class)->group(function () {
 	Route::prefix('blog')->group(function () {
 		Route::get('/', 'index')->name('blog.index');
 		Route::get('/{blog}', 'show')->name('blog.show');
-		Route::post('/', 'store')->name('blog.store');
-		Route::put('/{blog}', 'put')->name('blog.put');
-		Route::delete('/{blog}', 'destroy')->name('blog.destroy');
+		Route::middleware('auth:sanctum')->group(function () {
+			Route::post('/', 'store')->name('blog.store');
+			Route::put('/{blog}', 'put')->name('blog.put');
+			Route::delete('/{blog}', 'destroy')->name('blog.destroy');
+		});
 	});
 });
 
@@ -35,28 +37,36 @@ Route::controller(BlogCollectionController::class)->group(function () {
 	Route::prefix('collection')->group(function () {
 		Route::get('/', 'index')->name('collection.index');
 		Route::get('/{user:name}', 'show')->name('collection.show');
-		Route::post('/', 'store')->name('collection.store');
-		Route::put('/{collection}', 'put')->name('collection.put');
-		Route::delete('/{collection}', 'destroy')->name('collection.destroy');
+		Route::middleware('auth:sanctum')->group(function () {
+			Route::post('/', 'store')->name('collection.store');
+			Route::put('/{collection}', 'put')->name('collection.put');
+			Route::delete('/{collection}', 'destroy')->name('collection.destroy');
+		});
 	});
 });
 
 Route::controller(SubscribeController::class)->group(function () {
 	Route::prefix('subscribe')->group(function () {
 		Route::get('/', 'index')->name('subscribe.index');
-		Route::post('/', 'subscribeOrUnsubscribe')->name('subscribe.subscribe_or_unsubscribe');
+		Route::middleware('auth:sanctum')->group(function () {
+			Route::post('/', 'subscribeOrUnsubscribe')->name('subscribe.subscribe_or_unsubscribe');
+		});
 	});
 });
 
 Route::controller(LikeController::class)->group(function () {
 	Route::prefix('like')->group(function () {
 		Route::get('/', 'index')->name('like.index');
-		Route::post('/', 'likeOrUnlike')->name('like.like_or_unlike');
+		Route::middleware('auth:sanctum')->group(function () {
+			Route::post('/', 'likeOrUnlike')->name('like.like_or_unlike');
+		});
 	});
 });
 
 Route::controller(CommentController::class)->group(function () {
 	Route::prefix('/comment')->group(function () {
-		Route::post('/', 'store')->name('comment.store');
+		Route::middleware('auth:sanctum')->group(function () {
+			Route::post('/', 'store')->name('comment.store');
+		});
 	});
 });
