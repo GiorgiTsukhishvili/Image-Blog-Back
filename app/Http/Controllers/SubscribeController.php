14ef6ecb00101ff8ed61c0f596b2e19784bc714f
Giorgi Tsukhishvili <gcuxishvili@gmail.com>
2023-08-26 20:27:33 +0400

    fix: change of fetching subscribers

--- app/Http/Controllers/SubscribeController.php
+++ app/Http/Controllers/SubscribeController.php
@@ -10,9 +10,9 @@ class SubscribeController extends Controller
 {
 	public function index(): JsonResponse
 	{
-		$subscribers = Subscribe::where('user_id', auth()->user()->id)
-		->with('subscribes:id,name,image')
-		->get(['id', 'subscribed_id']);
+		$subscribers = Subscribe::where('subscribed_id', auth()->user()->id)
+		->with('user:id,name,image')
+		->get(['id', 'subscribed_id', 'user_id']);
 
 		return response()->json(['subscribers' => $subscribers], 200);
 	}
