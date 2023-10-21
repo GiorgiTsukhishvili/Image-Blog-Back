59d575f23e9a9d390d2c25df6dd420d3e7733f70
Giorgi Tsukhishvili <gcuxishvili@gmail.com>
2023-10-21 14:09:28 +0400

    feat: add model notifications relationships

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -4,8 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Notification extends Model
 {
-    use HasFactory;
+	use HasFactory;
+
+	protected $fillable = [
+		'user_id',
+		'creator_id',
+		'type',
+		'is_new',
+		'blog_id',
+	];
+
+	public function user(): BelongsTo
+	{
+		return $this->belongsTo(User::class);
+	}
+
+	public function creator(): BelongsTo
+	{
+		return $this->belongsTo(User::class, 'creator_id', 'id');
+	}
+
+	public function blog(): BelongsTo
+	{
+		return $this->belongsTo(Blog::class);
+	}
 }
--- database/migrations/2023_10_20_130740_create_notifications_table.php
+++ database/migrations/2023_10_20_130740_create_notifications_table.php
@@ -4,23 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    public function up(): void
-    {
-        Schema::create('notifications', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('user_id')->references('id')->on('users')->cascadeOnDelete();
-            $table->foreignId('creator_id')->references('id')->on('users')->cascadeOnDelete();
-            $table->enum('type', ['comment', 'like', 'subscribed', 'post']);
-            $table->boolean('is_new')->default(true);
-            $table->foreignId('blog_id')->references('id')->on('blogs')->cascadeOnDelete();
-            $table->timestamps();
-        });
-    }
+return new class extends Migration {
+	public function up(): void
+	{
+		Schema::create('notifications', function (Blueprint $table) {
+			$table->id();
+			$table->foreignId('user_id')->references('id')->on('users')->cascadeOnDelete();
+			$table->foreignId('creator_id')->references('id')->on('users')->cascadeOnDelete();
+			$table->enum('type', ['comment', 'like', 'subscribed', 'post']);
+			$table->boolean('is_new')->default(true);
+			$table->foreignId('blog_id')->references('id')->on('blogs')->nullable()->cascadeOnDelete();
+			$table->timestamps();
+		});
+	}
 
-    public function down(): void
-    {
-        Schema::dropIfExists('notifications');
-    }
+	public function down(): void
+	{
+		Schema::dropIfExists('notifications');
+	}
 };
