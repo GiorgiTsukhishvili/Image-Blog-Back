92fe61232d286bf8d9ebdf3ba6da94c7689199b4
Giorgi Tsukhishvili <gcuxishvili@gmail.com>
2024-01-14 15:04:44 +0400

    chore: add broadcast as in user notifications

--- app/Events/UserNotification.php
+++ app/Events/UserNotification.php
@@ -2,27 +2,28 @@
 
 namespace App\Events;
 
-use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
 class UserNotification
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+	use Dispatchable, InteractsWithSockets, SerializesModels;
 
-    public function __construct()
-    {
-        //
-    }
+	public function __construct()
+	{
+	}
 
-    public function broadcastOn(): array
-    {
-        return [
-            new PrivateChannel('image-notifications.' . auth()->user()->id),
-        ];
-    }
+	public function broadcastOn(): array
+	{
+		return [
+			new PrivateChannel('image-notifications.' . auth()->user()->id),
+		];
+	}
+
+	public function broadcastAs(): string
+	{
+		return 'notifications';
+	}
 }
